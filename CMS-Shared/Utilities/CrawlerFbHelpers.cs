56d067093ed783817a002d9c33f7733973a736a3
TuanVo <voquoctuanqn@gmail.com>
2018-07-04 15:41:57 +0700

    [M] update crawler

--- CMS-Shared/Utilities/CrawlerFbHelpers.cs
+++ CMS-Shared/Utilities/CrawlerFbHelpers.cs
@@ -26,6 +26,10 @@ namespace CMS_Shared.Utilities
                 httpWebRequest.UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:61.0) Gecko/20100101 Firefox/61.0";
                 httpWebRequest.Accept = "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8";
                 httpWebRequest.Headers["Proxy-Authorization"] = "Digest username=\"54737357\", realm=\"anonymox.net\", nonce=\"rt86WwAAAABgQKLm21UAAAwL3yIAAAAA\", uri=\"www.facebook.com:443\", response=\"47dc76deffbdaef3fc92784579b19d65\", qop=auth, nc=0000021f, cnonce=\"d0a10718bc5ea4b9\"";
+                //httpWebRequest.ServicePoint.BindIPEndPointDelegate = delegate
+                //{
+                //    return new IPEndPoint(IPAddress.Parse("146.185.28.59"), 443);
+                //};
                 //httpWebRequest.Headers["Remote-Address"] = "146.185.28.59:443";
                 httpWebRequest.Timeout = 100000;
                 var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
@@ -44,21 +48,21 @@ namespace CMS_Shared.Utilities
 
                     // fb_id
                     var nodeFb_Id = doc.DocumentNode.Descendants().Where
-                                        (
-                                            x => (x.Name == "span" && x.Attributes["class"] != null && x.Attributes["class"].Value.Contains("_3-8s"))
-                                        ).ToList();
+                                    (
+                                        x => (x.Name == "span" && x.Attributes["class"] != null && x.Attributes["class"].Value.Contains("_3-8s"))
+                                    ).ToList();
                     List<string> fb_ids = new List<string>();
-                    if(nodeFb_Id != null && nodeFb_Id.Any())
+                    if(nodeFb_Id != null && nodeFb_Id.Count > 0)
                     {
                         foreach(var item in nodeFb_Id)
                         {
                             var strfb_id = item.GetAttributeValue("id", "");
                             if(!string.IsNullOrEmpty(strfb_id))
                             {
-                                var splits = strfb_id.Split('_').ToList();
-                                if(splits != null && splits.Count >= 1)
+                                var split = strfb_id.Split('_').ToList();
+                                if(split != null && split.Count > 1)
                                 {
-                                    var fb_id = splits[1];
+                                    var fb_id = split[1];
                                     fb_ids.Add(fb_id);
                                 }
                             }
@@ -75,7 +79,7 @@ namespace CMS_Shared.Utilities
                         var index = 0;
                         foreach (var item in nodeHtmlImage)
                         {
-                            var fb_id = fb_ids[index];
+                            string fb_id = "";//fb_ids[index];
                             var nodeChildImage = item.Descendants("a").ToList();
                             if (nodeChildImage != null && nodeChildImage.Count > 0)
                             {
@@ -91,12 +95,19 @@ namespace CMS_Shared.Utilities
                                     var Pin = new PinsModels();
                                     if (!string.IsNullOrEmpty(_apiDetail))
                                     {
+                                        var Splits = _apiDetail.Split('/').ToList();
+                                        if (Splits != null && Splits.Count >= 5)
+                                            fb_id = Splits[4];
+
+
+                                        if (nodeChildImage.Count == 1)
+                                            fb_id = fb_ids[index];
+                                       
                                         CrawlerFBDetail(_apiDetail, fb_id, ref Pin);
                                     }
                                     Pin.ImageURL = _image;
                                     pins.Pins.Add(Pin);
                                 }
-                                
                             }
                             index++;
                         }
@@ -137,43 +148,7 @@ namespace CMS_Shared.Utilities
                         var nodeJson = script[10].InnerText;
                         if (!string.IsNullOrEmpty(nodeJson))
                         {
-                            //nodeJson = nodeJson.Replace("require(\"TimeSlice\")", "")
-                            //                   .Replace(".guard(function(){", "")
-                            //                   .Replace("},","")
-                            //                   .Replace("\"ServerJS define\",{\"root\": true })();","")
-                            //                   .Replace("require(\"ServerJSDefine\")", "")
-                            //                   .Replace(".handleDefines(", "")
-                            //                   .Replace(";require(\"InitialJSLoader\")", "")
-                            //                   .Replace(".handleServerJS({", "");
-                           // var jsonfeedbacktarget =  findElement(nodeJson, "feedbacktarget", 0);
-                            findNode(nodeJson, "feedbacktarget", 0, fb_id);
-                            //JavaScriptSerializer jsonSerializer = new JavaScriptSerializer();
-                            //dynamic dobj = jsonSerializer.Deserialize<dynamic>(jsonfeedbacktarget);
-                            //var dictionary = dobj as Dictionary<string, dynamic>;
-                            //if (dictionary.ContainsKey("entidentifier"))
-                            //{
-                            //    var _fb_id = dictionary["entidentifier"];
-                            //    if (fb_id.Equals(_fb_id))
-                            //    {
-                            //        if (dictionary.ContainsKey("commentTotalCount"))
-                            //        {
-                            //            var commentTotalCount = Convert.ToInt16(dictionary["commentTotalCount"]);
-                            //        }
-                            //        if (dictionary.ContainsKey("reactioncount"))
-                            //        {
-                            //            var reactioncount = Convert.ToInt16(dictionary["reactioncount"]);
-                            //        }
-                            //        if (dictionary.ContainsKey("sharecount"))
-                            //        {
-                            //            var sharecount = Convert.ToInt16(dictionary["sharecount"]);
-                            //        }
-                            //    }
-                            //    else
-                            //    {
-                            //        jsonfeedbacktarget = "\"feedbacktarget\":" + jsonfeedbacktarget;
-                            //        nodeJson = nodeJson.Replace(jsonfeedbacktarget, "");
-                            //    }
-                            //}
+                             findNode(nodeJson, "feedbacktarget", 0, fb_id, ref pin);
                         }
                     }
                 }
@@ -184,7 +159,7 @@ namespace CMS_Shared.Utilities
             }
         }
 
-        public static void findNode(string input, string key, int start,string fb_id)
+        public static void findNode(string input, string key, int start,string fb_id, ref PinsModels pin)
         {
             var jsonfeedbacktarget = findElement(input, "feedbacktarget", 0);
             JavaScriptSerializer jsonSerializer = new JavaScriptSerializer();
@@ -197,22 +172,22 @@ namespace CMS_Shared.Utilities
                 {
                     if (dictionary.ContainsKey("commentTotalCount"))
                     {
-                        var commentTotalCount = Convert.ToInt16(dictionary["commentTotalCount"]);
+                         pin.commentTotalCount = Convert.ToInt16(dictionary["commentTotalCount"]);
                     }
                     if (dictionary.ContainsKey("reactioncount"))
                     {
-                        var reactioncount = Convert.ToInt16(dictionary["reactioncount"]);
+                        pin.reactioncount = Convert.ToInt16(dictionary["reactioncount"]);
                     }
                     if (dictionary.ContainsKey("sharecount"))
                     {
-                        var sharecount = Convert.ToInt16(dictionary["sharecount"]);
+                        pin.sharecount = Convert.ToInt16(dictionary["sharecount"]);
                     }
                 }
                 else
                 {
                     jsonfeedbacktarget = "\"feedbacktarget\":" + jsonfeedbacktarget;
                     input = input.Replace(jsonfeedbacktarget, "");
-                    findNode(input, key, start, fb_id);
+                    findNode(input, key, start, fb_id,ref pin);
                 }
             }
         }
